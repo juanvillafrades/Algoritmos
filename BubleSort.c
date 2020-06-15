6549b7af9c73239d86ecdc12319d459ff78157a0
svillafrades <svillafrades@gmail.com>
2020-06-15 12:59:36 -0500

    agregue la funcion main

--- BubleSort.c
+++ BubleSort.c
@@ -7,7 +7,7 @@ void ChangePos(int *n1, int *n2){
     *n2 = temp;
 }
 
-void BubleSort(int InVector[], n){
+void BubleSort(int InVector[], int n){
     int i, j;
     for(i =; i < n-1; i++){
         for(j=0; j < n-i-1; j++){
@@ -15,4 +15,23 @@ void BubleSort(int InVector[], n){
                 ChangePos(&InVector[j],&InVector[j+1]);
         }
     }
+}
+
+int print(InVector[j], int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        printf("%d ,", InVector[i]);
+        printf("/n fin del ordenamiento")
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int InVector[]= {100, 1992, 0, 5, -1, 60, 70, 14, 15, 10};
+    int n = sizeof(InVector)/sizeof(InVector[0]);
+    BubleSort(InVector, n);
+    print(InVector, n);
+    printf("/n");
+    return 0;
 }

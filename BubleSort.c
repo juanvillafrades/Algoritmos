1223fb887b0f7946a8688aa079d8f8c247b7baa8
svillafrades <svillafrades@gmail.com>
2020-06-15 11:36:12 -0500

    void funtions

--- BubleSort.c
+++ BubleSort.c
@@ -1,3 +1,18 @@
 // Algorit,o de ordenamiento Buble sort
 #include<stdio.h>
 
+void ChangePos(int *n1, int *n2){
+    int temp = *n1;
+    *n1 = *n2;
+    *n2 = temp;
+}
+
+void BubleSort(int InVector[], n){
+    int i, j;
+    for(i =; i < n-1; i++){
+        for(j=0; j < n-i-1; j++){
+            if(InVector[j] > InVector[j+1])
+                ChangePos(&InVector[j],&InVector[j+1]);
+        }
+    }
+}
